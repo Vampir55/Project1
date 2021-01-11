63589321f626bdd724af8dc64425a5dca0cccb9d
Vampir55 <ncr2005@mail.ru>
2021-01-12 02:19:26 +0500

    Example1

--- Example_xls.py
+++ Example_xls.py
@@ -4,7 +4,9 @@ import openpyxl
 
 workbook = openpyxl.load_workbook(filename="Data1.xlsx")  # load xls file
 sheet = workbook['Лист1']
+sheet2 = workbook.create_sheet("List2")
 
-val = sheet['A1'].value
-
-print(val)
+for row in range(1, 20):
+    sheet2.cell(row, 1, value=sheet.cell(row, 3).value)
+    print(sheet2.cell(row, 1).value)
+print("End lines")
